e43a2250d5b93bb2ced4b782b5678d0e5b0a9ca9
saltchicken <john.eicher89@gmail.com>
2024-04-07 14:54:32 -0700

    Increase interuption time

--- server/deferred_server.py
+++ server/deferred_server.py
@@ -55,7 +55,7 @@ class ClientProtocol(basic.LineReceiver):
                 self.m.associatedThread = d
                 self.m.addCallback(lambda result: print("Result obtained:", result))
                 self.m.addErrback(handle_cancellation)
-                reactor.callLater(1, cancel_computation, self.m)
+                reactor.callLater(3, cancel_computation, self.m)
                 
 
 class TalonFactory(protocol.Factory):
