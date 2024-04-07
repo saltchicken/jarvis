0b48bbba6ab53060564bd779a5515c747080c4a3
saltchicken <john.eicher89@gmail.com>
2024-04-07 16:20:48 -0700

    Test splitting up the ClientProtocol class

--- server/server.py
+++ server/server.py
@@ -9,7 +9,7 @@ from server.classes import PhraseMessage, SystemMessage, JSONMessage
 from loguru import logger
 
 
-class ClientProtocol(basic.LineReceiver):
+class TalonProtocol(basic.LineReceiver):
     def __init__(self, factory):
         self.factory = factory
         self.d = None
@@ -65,6 +65,62 @@ class ClientProtocol(basic.LineReceiver):
                         self.factory.d.cancel()
                 elif message.message == 'clear':
                     reactor.callLater(0, threads.deferToThread, self.sendSystemMessage, 'clear')
+                    
+class TaskerProtocol(basic.LineReceiver):
+    def __init__(self, factory):
+        self.factory = factory
+        self.d = None
+
+    def connectionMade(self):
+        logger.debug(f"{self.factory.name} connected")
+        self.factory.client = self
+
+    def connectionLost(self, reason):
+        logger.debug(f"{self.factory.name} disconnected")
+            
+    def runLLM(self, data, deferred):
+        output = ''
+        for chunk in self.factory.chain.stream(data):
+            try:
+                output += chunk
+                message = PhraseMessage(message=output)
+                self.send(message)
+                # logger.debug(deferred.called)
+                if deferred.called:
+                    deferred.callback(output)
+                    break
+            except Exception as e:
+                logger.error(e)
+        # TODO: Make a function explicitly for calling after LLM stream is done
+        logger.debug('LLM thread complete')
+        # reactor.callLater(6, threads.deferToThread, self.sendSystemMessage, 'clear')
+    
+    def send(self, message):
+        self.factory.tasker.client.sendLine(message.dump.encode())
+        
+    def sendSystemMessage(self, system_message):
+        logger.debug('sending system message')
+        self.send(SystemMessage(message=system_message))
+        
+
+    def dataReceived(self, data):
+        logger.debug(f"{self.factory.name} received data: {data}")
+        if self.factory.name == "Talon":
+            message = JSONMessage(dump=data)
+            if message.type == 'phrase':
+                self.factory.d = defer.Deferred()
+                t = threads.deferToThread(self.runLLM, message.message, self.factory.d)
+                self.factory.d.associatedThread = t
+                self.factory.d.addCallback(lambda result: print("Result obtained:", result)) # TODO: This never calls
+                self.factory.d.addErrback(lambda result: print(f"Cancellation Received"))
+            elif message.type == 'command':
+                if message.message == "stop":
+                    if self.factory.d == None:
+                        logger.warning('self.d is None')
+                    else:
+                        self.factory.d.cancel()
+                elif message.message == 'clear':
+                    reactor.callLater(0, threads.deferToThread, self.sendSystemMessage, 'clear')
                 
 
 class TalonFactory(protocol.Factory):
@@ -75,7 +131,7 @@ class TalonFactory(protocol.Factory):
         self.d = None
 
     def buildProtocol(self, addr):
-        return ClientProtocol(self)
+        return TalonProtocol(self)
     
 class TaskerFactory(protocol.Factory):
     def __init__(self):
@@ -83,7 +139,7 @@ class TaskerFactory(protocol.Factory):
         self.client = None
 
     def buildProtocol(self, addr):
-        return ClientProtocol(self)
+        return TaskerProtocol(self)
 
 def main():
     talon_port = 8000
